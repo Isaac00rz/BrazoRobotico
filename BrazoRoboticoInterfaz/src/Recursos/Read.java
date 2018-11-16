e523344e8fc2f0e1c6fb4925bef66785ebf5cb17
IsaacPerezSegura <15240485@itleon.edu.mx>
2018-11-15 23:22:48 -0600

    Lectura y escritura

--- BrazoRoboticoInterfaz/src/Recursos/Read.java
+++ BrazoRoboticoInterfaz/src/Recursos/Read.java
@@ -1,5 +1,24 @@
 package Recursos;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Read {
-    
+    private String text="";
+    private String route;
+
+    public String read(String route){
+      int caracter;
+      this.route=route;
+      text="";
+      try{
+        FileReader file = new FileReader(route);
+        BufferedReader buffer = new BufferedReader(file);
+        while((caracter=buffer.read())!=-1){
+            text+=((char)caracter);
+        }
+        buffer.close();
+      }catch(Exception e){System.out.println(e);}
+      return text;
+    }
 }
--- BrazoRoboticoInterfaz/src/Recursos/Write.java
+++ BrazoRoboticoInterfaz/src/Recursos/Write.java
@@ -1,5 +1,24 @@
 package Recursos;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Write {
-    
+    private String text="";
+    private String route;
+
+    public void write(String content){
+        PrintWriter writer;
+        try {
+            writer = new PrintWriter("moves.txt", "UTF-8");
+            writer.println(content);
+            writer.close();
+        } catch (FileNotFoundException | UnsupportedEncodingException ex) {
+            Logger.getLogger(Write.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
 }
