899379a9e7950e807de4e74d6399cce42fa938bc
kohire <42823603+kohire@users.noreply.github.com>
2018-11-19 21:34:54 -0600

    Agregación leds y buzzer.
    Se agregan leds para saber qué servo es el que se está moviendo junto a un pequeño sonido.

--- BrazoRobotico/BrazoRobotico.ino
+++ BrazoRobotico/BrazoRobotico.ino
@@ -1,258 +1,301 @@
 #include<EEPROM.h>
 #include <Servo.h>
 
-int pinBoton = 2;
+const int pinBoton = 2; //Botón de paro.
+const int buzzer = 13;
+// Leds que nos ayudarán a saber qué servo se está moviendo.
+const int ledWh = 9;
+const int ledGr = 10;
+const int ledOr = 11;
+const int ledYe = 12;
+const int ledRed = 4;
+// Declaramos los servos.
 Servo servo1;
 Servo servo2;
 Servo servo3;
 Servo servo4;
-Servo servo5; 
+Servo servo5;
 char instruccion[61];
 const int timeThreshold = 150;
 long timeCounter = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(pinBoton,INPUT);
-  servo1.attach(4);
-  servo2.attach(5);
-  servo3.attach(6);
-  servo4.attach(7);
-  servo5.attach(8);
-  attachInterrupt(digitalPinToInterrupt(pinBoton), parar,LOW);
+  pinMode(pinBoton, INPUT);
+  pinMode(buzzer, OUTPUT);
+  noTone(buzzer);
+  pinMode(ledWh, OUTPUT);
+  pinMode(ledGr, OUTPUT);
+  pinMode(ledOr, OUTPUT);
+  pinMode(ledYe, OUTPUT);
+  pinMode(ledRed, OUTPUT);
+  servo1.attach(4); //Servo 360.
+  servo2.attach(5); // Cadera.
+  servo3.attach(6); // Codo.
+  servo4.attach(7); //Muñeca.
+  servo5.attach(8); //Pinza.
+  attachInterrupt(digitalPinToInterrupt(pinBoton), parar, LOW); //Si llega a suceder algo que no haga el brazo, cortamos el circuito por el pin 2 para detenerlo.
 }
 
 void loop() {
-  int conLeer=0;
+  noTone(buzzer);
+  digitalWrite(ledWh, LOW);
+  digitalWrite(ledGr, LOW);
+  digitalWrite(ledOr, LOW);
+  digitalWrite(ledYe, LOW);
+  digitalWrite(ledRed, LOW);
+  int conLeer = 0;
   int i = 0;
   int valAnterior = 1;
   char modo[1];
-  EEPROM.get(1000,modo);
-  if(modo[0]=='1'){
-    EEPROM.get(0,instruccion);
-    EEPROM.get(1001,valAnterior);
-    for(i=valAnterior;i<61;i=i+2){
-      if(instruccion[i] == '@'){
-        EEPROM.put(1001,1);
+  EEPROM.get(1000, modo);
+  if (modo[0] == '1') {
+    EEPROM.get(0, instruccion);
+    EEPROM.get(1001, valAnterior);
+    for (i = valAnterior; i < 61; i = i + 2) {
+      if (instruccion[i] == '@') {
+        EEPROM.put(1001, 1);
         break;
-      }else{
-        switch(instruccion[i]){
-        case '1':
-        moverServo1(i);
-        break;
-        case '2':
-        moverServo2(i);
-        break;
-        case '3':
-        moverServo3(i);
-        break;
-        case '4':
-        moverServo4(i);
-        break;
-        case '5':
-        moverServo5(i);
-        break;
+      } else {
+        switch (instruccion[i]) {
+          case '1':
+            moverServo1(i);
+            break;
+          case '2':
+            moverServo2(i);
+            break;
+          case '3':
+            moverServo3(i);
+            break;
+          case '4':
+            moverServo4(i);
+            break;
+          case '5':
+            moverServo5(i);
+            break;
         }
       }
     }
-  }else if (modo[0]=='0'){
-    if(Serial.available()>0){
-    delay(100);
-    while(Serial.available()>0){
-      instruccion[conLeer]=Serial.read();
-      conLeer++;
+  } else if (modo[0] == '0') {
+    if (Serial.available() > 0) {
+      delay(100);
+      while (Serial.available() > 0) {
+        instruccion[conLeer] = Serial.read();
+        conLeer++;
+      }
+      if (instruccion[0] == '0') {
+        EEPROM.put(1000, '0');
+        switch (instruccion[1]) {
+          case '1':
+            moverServo1(1);
+            break;
+          case '2':
+            moverServo2(1);
+            break;
+          case '3':
+            moverServo3(1);
+            break;
+          case '4':
+            moverServo4(1);
+            break;
+          case '5':
+            moverServo5(1);
+            break;
+        }
+      } else if (instruccion[0] == '1') {
+        EEPROM.put(1000, '1');
+        EEPROM.put(1001, 1);
+        EEPROM.put(0, instruccion);
+      }
     }
-    if(instruccion[0] == '0'){
-      EEPROM.put(1000,'0');
-      switch(instruccion[1]){
-        case '1':
-        moverServo1(1);
-        break;
-        case '2':
-        moverServo2(1);
-        break;
-        case '3':
-        moverServo3(1);
-        break;
-        case '4':
-        moverServo4(1);
-        break;
-        case '5':
-        moverServo5(1);
-        break;
-      }
-      }else if(instruccion[0] == '1'){
-        EEPROM.put(1000,'1');
-        EEPROM.put(1001,1);
-        EEPROM.put(0,instruccion);
-      }
   }
- }
 }
 
-void parar(){
+void parar() {
   int sig;
-  if(millis() > timeCounter + timeThreshold){
-    EEPROM.put(1000,'0');
-    EEPROM.get(1001,sig);
+  if (millis() > timeCounter + timeThreshold) {
+    EEPROM.put(1000, '0');
+    EEPROM.get(1001, sig);
     instruccion[sig] = '@';
+    digitalWrite(ledRed, HIGH);
   }
-  
 }
 
-void moverServo1(int ultimo){
-  int valor = calValor(instruccion[ultimo+1]);
+void moverServo1(int ultimo) {
+  int valor = calValor(instruccion[ultimo + 1]);
   servo1.write(valor);
-  EEPROM.put(1001,(ultimo+2));              
-  delay(1000);
+  EEPROM.put(1001, (ultimo + 2));
+  delay(1500);
+  tone(buzzer, 500);
+  delay(500);
+  noTone(buzzer);
 }
-void moverServo2(int ultimo){
-  int valor = calValor(instruccion[ultimo+1]);
-  servo2.write(valor); 
-  EEPROM.put(1001,(ultimo+2));   
-  delay(1000);                     
+void moverServo2(int ultimo) {
+  int valor = calValor(instruccion[ultimo + 1]);
+  servo2.write(valor);
+  EEPROM.put(1001, (ultimo + 2));
+  delay(1500);
+  digitalWrite(ledWh, HIGH);
+  delay(500);
+  tone(buzzer, 600);
+  delay(500);
+  noTone(buzzer);
 }
 
-
-void moverServo3(int ultimo){
-  int valor = calValor(instruccion[ultimo+1]);
-  servo3.write(valor);  
-  EEPROM.put(1001,(ultimo+2));  
-  delay(1000);
+void moverServo3(int ultimo) {
+  int valor = calValor(instruccion[ultimo + 1]);
+  servo3.write(valor);
+  EEPROM.put(1001, (ultimo + 2));
+  delay(1500);
+  digitalWrite(ledGr, HIGH);
+  delay(500);
+  tone(buzzer, 700);
+  delay(500);
+  noTone(buzzer);
 }
 
-void moverServo4(int ultimo){
-  int valor = calValor(instruccion[ultimo+1]);
-  servo4.write(valor);  
-  EEPROM.put(1001,(ultimo+2));  
-  delay(100);
+void moverServo4(int ultimo) {
+  int valor = calValor(instruccion[ultimo + 1]);
+  servo4.write(valor);
+  EEPROM.put(1001, (ultimo + 2));
+  delay(1500);
+  digitalWrite(ledOr, HIGH);
+  delay(500);
+  tone(buzzer, 800);
+  delay(500);
+  noTone(buzzer);
 }
-void moverServo5(int ultimo){
-  int valor = calValor(instruccion[ultimo+1]);
-  if(valor==60){
-    valor=64;
-  }else if(valor==110){
+void moverServo5(int ultimo) {
+  int valor = calValor(instruccion[ultimo + 1]);
+  if (valor == 60) {
+    valor = 64;
+  } else if (valor == 110) {
     valor = 105;
   }
-  servo5.write(valor);  
-  EEPROM.put(1001,(ultimo+2));  
-  delay(1000);
+  servo5.write(valor);
+  EEPROM.put(1001, (ultimo + 2));
+  delay(1500);
+  digitalWrite(ledYe, HIGH);
+  delay(500);
+  tone(buzzer, 900);
+  delay(500);
+  noTone(buzzer);
 }
 
-int calValor(char letra){
+int calValor(char letra) {
   int va = 0;
-  switch(letra){
+  switch (letra) {
     case 'a':
-    va = 0;
-    break;
+      va = 0;
+      break;
     case 'b':
-    va = 10;
-    break;
+      va = 10;
+      break;
     case 'c':
-    va = 20;
-    break;
+      va = 20;
+      break;
     case 'd':
-    va = 30;
-    break;
+      va = 30;
+      break;
     case 'e':
-    va = 40;
-    break;
+      va = 40;
+      break;
     case 'f':
-    va = 50;
-    break;
+      va = 50;
+      break;
     case 'g':
-    va = 60;
-    break;
+      va = 60;
+      break;
     case 'h':
-    va = 70;
-    break;
+      va = 70;
+      break;
     case 'i':
-    va = 80;
-    break;
+      va = 80;
+      break;
     case 'j':
-    va = 90;
-    break;
+      va = 90;
+      break;
     case 'k':
-    va = 100;
-    break;
+      va = 100;
+      break;
     case 'l':
-    va = 110;
-    break;
+      va = 110;
+      break;
     case 'm':
-    va = 120;
-    break;
+      va = 120;
+      break;
     case 'n':
-    va = 130;
-    break;
+      va = 130;
+      break;
     case 'o':
-    va = 140;
-    break;
+      va = 140;
+      break;
     case 'p':
-    va = 150;
-    break;
+      va = 150;
+      break;
     case 'q':
-    va = 160;
-    break;
+      va = 160;
+      break;
     case 'r':
-    va = 170;
-    break;
+      va = 170;
+      break;
     case 's':
-    va = 180;
-    break;
+      va = 180;
+      break;
     case 't':
-    va = 190;
-    break;
+      va = 190;
+      break;
     case 'u':
-    va = 200;
-    break;
+      va = 200;
+      break;
     case 'v':
-    va = 210;
-    break;
+      va = 210;
+      break;
     case 'x':
-    va = 220;
-    break;
+      va = 220;
+      break;
     case 'y':
-    va = 230;
-    break;
+      va = 230;
+      break;
     case 'z':
-    va = 240;
-    break;
+      va = 240;
+      break;
     case 'A':
-    va = 250;
-    break;
+      va = 250;
+      break;
     case 'B':
-    va = 260;
-    break;
+      va = 260;
+      break;
     case 'C':
-    va = 270;
-    break;
+      va = 270;
+      break;
     case 'D':
-    va = 280;
-    break;
+      va = 280;
+      break;
     case 'E':
-    va = 290;
-    break;
+      va = 290;
+      break;
     case 'F':
-    va = 300;
-    break;
+      va = 300;
+      break;
     case 'G':
-    va = 310;
-    break;
+      va = 310;
+      break;
     case 'H':
-    va = 320;
-    break;
+      va = 320;
+      break;
     case 'I':
-    va = 330;
-    break;
+      va = 330;
+      break;
     case 'J':
-    va = 340;
-    break;
+      va = 340;
+      break;
     case 'K':
-    va = 350;
-    break;
+      va = 350;
+      break;
     case 'L':
-    va = 360;
-    break;
+      va = 360;
+      break;
   }
   return va;
 }
